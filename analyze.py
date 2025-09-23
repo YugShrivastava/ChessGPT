c4f434a56c6e13e53f0c6c00dbb29c58cbc8f5c1
sanyam280 <sanyam.code280@gmail.com>
2025-09-23 15:32:14 +0530

    modified tactic.py

--- analyze.py
+++ analyze.py
@@ -7,17 +7,16 @@ load_dotenv()
 
 def get_analysis(fen_before: str, fen_after):
     tactical_analyzer = TacticalAnalyzer()
-    tactical_analyzer.analyze(fen_before, fen_after, time_limit=1.0, multipv=1)
-    analysis = tactical_analyzer.get_analysis()[0]
+    analysis=tactical_analyzer.analyze(fen_before, fen_after, time_limit=1.0, multipv=1)
+    # analysis = tactical_analyzer.get_analysis()[0]
 
-    if analysis["tactic"]:
-        extractor = ChessFeatureExtractor()
-        analysis["postional_features"] = {}
-        analysis["positional_features"] = extractor.extract_features(fen_after)
+    extractor = ChessFeatureExtractor()
+    analysis["postional_features"] = {}
+    analysis["positional_features"] = extractor.extract_features(fen_after)
 
     tactical_analyzer.quit()
 
     return analysis
 
 if __name__ == "__main__":
-    print(get_analysis("r2qk2r/1p2bppp/p1n1p1n1/2PpP3/BP6/5N1P/P1P2PP1/R1BQK2R w KQkq - 1 12", "r2qk2r/4bppp/ppn1p1n1/2PpP3/BP6/P4N1P/2P2PP1/R1BQK2R w KQkq - 0 13"))
+    print(get_analysis("r5k1/1p3pp1/2pBpb1p/2P4P/1q1P4/2nQ4/P2R1PP1/R5K1 b - - 11 33","r5k1/1p3pp1/2pBpb1p/2P4P/1q1Pn3/3Q4/P2R1PP1/R5K1 w - - 12 34"))
